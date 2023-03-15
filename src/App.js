51ddbe67d06fa9c16d50d129d283e38cf1e630f6
Rashad Adams <Rashaddms@gmail.com>
2023-03-15 13:31:38 -0400

    adding first functionality to display list of notes
    Replaced
    import 'antd/dist/antd.css'
    
    with
    
    import 'antd/dist/reset.css'
    
     to resolve error

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useReducer } from 'react';
+import { API } from 'aws-amplify';
+import { List } from 'antd';
+import 'antd/dist/reset.css';
+import { listNotes } from './graphql/queries';
 
-function App() {
+// Variable object declaration
+const initialState = {
+  notes: [],
+  loading: true,
+  error: false,
+  form: { name: '', description: '' }
+};
+
+// Reducer function
+function reducer(state, action) {
+  switch(action.type) {
+    case 'SET_NOTES':
+        return { ...state, notes: action.notes, loading: false };
+    case 'ERROR':
+        return { ...state, loading: false, error: true };
+    default:
+        return { ...state };
+  };
+};
+
+const App = () => {
+
+  // state and dispatch variables created and useReducer called using reducer and initialState passed to it 
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  // fetch notes function created that will call the AppSync API and set the notes array once the APY call is successful 
+  const fetchNotes = async() => {
+    try {
+      const notesData = await API.graphql({
+        query: listNotes
+      })
+      dispatch({ type: 'SET_NOTES', notes: notesData.data.listNotes.items })
+    } catch (err) {
+      console.log('error: ', err)
+      dispatch({ type: 'ERROR' })
+    }
+  };
+
+  // eseEffect hook exececutes when the page first loads and sets to an empty array
+  useEffect(() => {
+    fetchNotes()
+  }, []);
+
+  // variable for styling
+  const styles = {
+    container: {padding: 20},
+    input: {marginBottom: 10},
+    item: { textAlign: 'left' },
+    p: { color: '#1890ff' }
+  }
+
+  // function calls every single item in the list
+  function renderItem(item) {
+      return (
+        <List.Item style={styles.item}>
+          <List.Item.Meta
+            title={item.name}
+            description={item.description}
+          />
+        </List.Item>
+      )
+    };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div style={styles.container}>
+    <List
+      loading={state.loading}
+      dataSource={state.notes}
+      renderItem={renderItem}
+    />
+  </div>
   );
 }
 
